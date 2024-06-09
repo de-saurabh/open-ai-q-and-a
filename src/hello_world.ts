b006c711a8f350fbab29145115cf26ced2e3f630
Saurabh <de_saurabh@outlook.com>
2024-06-09 11:45:05 +0530

    Fix query params

--- src/hello_world.ts
+++ src/hello_world.ts
@@ -3,7 +3,7 @@ import {Context} from "koa";
 export default class HelloWorldService {
     public static helloWorld(ctx: Context) {
         try {
-            const { name = 'World' } = ctx.request.body as { name: string};
+            const { name = 'World' } = ctx.params;
             return ctx.body = `Hello ${name}`;
         } catch (error) {
             throw error;
